22afe85b33d24f3279b2707eba9638b9e6e6d022
Your Name <you@example.com>
2017-05-08 21:17:33 +0200

    tabs2spaces

--- run.sh
+++ run.sh
@@ -2,16 +2,16 @@
 
 if [ ! -e $(which airmon-ng) ]
 then
-	echo "please install aircrack.. exiting"
+    echo "please install aircrack.. exiting"
 fi
 
 if [[ ! -z $1 && -e /sys/class/net/$1 ]]
 then
-	INTERFACE=$1
-	echo "using interface $INTERFACE"
+    INTERFACE=$1
+    echo "using interface $INTERFACE"
 else
-	echo "interface does not exist!.. exit"
-	exit -1
+    echo "interface does not exist!.. exit"
+    exit -1
 fi
 
 sudo airmon-ng start $INTERFACE
