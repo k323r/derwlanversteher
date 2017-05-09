573a30d1d144a21877e4cff74c4e9afc5677c50f
christian.backe <christian.backe@dfki.de>
2017-05-09 14:13:16 +0200

    completed cleanup of first log.py version: f_dump not global, no magic numbers, self-documenting code, docstrings (with TODOs), PEP-8, logfile overwrite option, autocreate data directory

--- src/log.py
+++ src/log.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import os.path
 import sys
 
@@ -7,53 +8,127 @@ from scapy.all import load_module, sniff, Dot11
 
 USAGE = """\
 usage: sudo python log.py [-f] <monitoring-interface> <logfile-name>
-example: sudo python log.py wlp1s0mon macs.dat"""
+example: sudo python log.py wlp1s0mon test.dat"""
 
-ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-DIR_DATA = os.path.join(ROOT, 'data')
+DIR_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+DIR_DATA = os.path.join(DIR_ROOT, 'data')
 
-### see the following link for package types
+# Package types and subtypes.
+# More information:
 # https://supportforums.cisco.com/document/52391/80211-frames-starter-guide-learn-wireless-sniffer-traces
+T_MANAGEMENT = 0
+ST_ASSOCIATION_REQUEST = 0
+ST_REASSOCIATION_REQUEST = 2
+ST_PROBE_REQUEST = 4
+ACCEPTED_SUBTYPES = [
+    ST_ASSOCIATION_REQUEST,
+    ST_REASSOCIATION_REQUEST,
+    ST_PROBE_REQUEST
+]
 
-ap_list = [] # set()
-subtype_list = []
-type_list = []
+def mkdir_p(path):
+    """
+    Wrapper around os.makedirs, except that it keeps silent if the path points
+    to an existing directory.
+    """
+    try:
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            raise OSError("Could not create directory: %s." % path)
 
-accepted_subtypes = [0, 2, 4]
+def get_command_line_parameters():
+    """
+    If the user supplies proper parameters, parse and return them in a format
+    compatible with Logger.__init__. Otherwise print USAGE information to
+    stdout and exit.
+    Note: I couldn't be bothered setting up argparse for one simple development
+    convenience flag. If more command line options should be desired in the
+    future, this decision might be reconsidered.
+    """
+    args = sys.argv
+    options = [arg for arg in args if arg.startswith('-')]
+    parameters = [arg for arg in args if not arg in options]
+    overwrite_logfile = ('-f' in options)
+    try:
+        monitoring_interface, logfile_name = parameters[1:3]
+    except:
+        print USAGE
+        sys.exit()
+    return monitoring_interface, logfile_name, overwrite_logfile
 
-# read command line parameters and maybe overwrite option
-args = sys.argv
-options = [arg for arg in args if arg.startswith('-')]
-parameters = [arg for arg in args if not arg in options]
-use_the_force = ('-f' in options)
-try:
-    interface, fname_log = parameters[1:3]
-except:
-    print USAGE
-    sys.exit()
+def is_acceptable(packet):
+    """
+    TODO: "is_acceptable" is OK for first tests, but too generic. Define a
+    series of specific boolean functions with appropriate names to reflect
+    different packet semantics. Then apply these inside Logger.handle_packet.
+    """
+    return (
+        packet.haslayer(Dot11)
+        and packet.type == T_MANAGEMENT
+        and packet.subtype in ACCEPTED_SUBTYPES)
 
-# init log file
-path_log = os.path.join(DIR_DATA, fname_log)
-if (not use_the_force) and os.path.exists(path_log):
-    print 'Log file exists already: %s. Exiting.' % path_log
-    sys.exit()
-f_dump = open(path_log, "w+")
-f_dump.writelines("MAC RSSI\n")
+class Logger(object):
+    """
+    Basically a wrapper around handle_packet to avoid a global logfile_handle.
+    Unfortunately, scapy.all.sniff seems to insist on managing the main loop
+    internally, instead of beeing managed by one.
+    """
+    def __init__(self, monitoring_interface, logfile_name, overwrite_logfile):
+        self.monitoring_interface = monitoring_interface
+        self.logfile_path = os.path.join(DIR_DATA, logfile_name)
+        self.overwrite_logfile = overwrite_logfile
+        self._init_log_file()
+        self._setup_sniffing()
 
-# What is p0f ?
-load_module("p0f")
+    def _init_log_file(self):
+        """
+        Create the DIR_DATA if necessary, then start a fresh log at
+        self.logfile_path -- unless it exists already and shall not be
+       overwritten, in which case we exit. Keep the logfile open for further
+        additions.
+        TODO: Refactor into database connect and update routine.
+        """
+        try:
+            mkdir_p(DIR_DATA)
+        except OSError as e:
+            print e
+            sys.exit()
+        if os.path.exists(self.logfile_path) and not self.overwrite_logfile:
+            print 'Log file exists already: %s. Exiting.' % self.logfile_path
+            sys.exit()
+        self.logfile_handle = open(self.logfile_path, "w+")
+        self.logfile_handle.write("mac rssi\n")
 
-def PacketHandler(pkt):
-    if pkt.haslayer(Dot11):
-        if pkt.type == 0 and pkt.subtype in accepted_subtypes:
-            if pkt.addr2 not in ap_list:
-                extra = pkt.notdecoded 
-                rssi = -(256-ord(extra[-4:-3]))
-                ap_list.append(pkt.addr2) # set --> add
-                print 'MAC adress %s strength (dBm) %s' %(pkt.addr2, rssi)
-                f_dump.writelines('%s %s \n' %(pkt.addr2, rssi))
-                f_dump.flush()
+    def _setup_sniffing(self):
+        """
+        Requisites for handle_packet.
+        """
+        load_module("p0f")
+        self.macs = set()
 
+    def handle_packet(self, packet):
+        """
+        TODO:
+            * Refactor using a database.
+            * Expand is_acceptable (see there).
+              (==> What else (besides .addr2) is interesting in a packet?)
+            * Explain rssi parsing magic.
+        """
+        if is_acceptable(packet):
+            # https://en.wikipedia.org/wiki/MAC_address
+            mac = packet.addr2
+            if not mac in self.macs:
+                # https://en.wikipedia.org/wiki/Received_signal_strength_indication
+                rssi = -(256-ord(packet.notdecoded[-4:-3]))
+                print 'MAC adress %s strength (dBm) %s' %(mac, rssi)
+                self.macs.add(mac)
+                self.logfile_handle.write('%s %s\n' %(mac, rssi))
+                self.logfile_handle.flush()
 
-sniff(iface = interface , prn = PacketHandler, store = 0)
-
+if __name__ == '__main__':
+    lgr = Logger(*get_command_line_parameters())
+    sniff(
+        iface=lgr.monitoring_interface, 
+        prn=lgr.handle_packet,
+        store=0)
